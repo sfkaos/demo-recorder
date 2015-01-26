e95b3abf7994bf1e2521a23ccba5d4892c6c9180
Peter Sobot <psobot@gmail.com>
2015-01-26 13:45:40 -0500

    Added example secrets file.

---
+++ Demo Recorder/Secrets.example.swift
@@ -0,0 +1,19 @@
+//
+//  Secrets.swift
+//  Demo Recorder
+//
+//  Created by Peter Sobot on 1/26/15.
+//  Copyright (c) 2015 The Working Group. All rights reserved.
+//
+//  Replace the constants in this file with your own constants
+//  before building this project.
+//
+
+import Foundation
+
+class Secrets {
+    class var AWSS3Bucket: NSString { return "mybucket" }
+    class var AWSS3KeyPrefix: NSString { return "foo/bar" }
+    class var AWSAccessKey: NSString { return "AKI-----------------" }
+    class var AWSSecretKey: NSString { return "-------------------------/-/------------" }
+}
