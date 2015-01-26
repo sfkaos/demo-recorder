76a29e84e2f918c79418dd1c96a9d5136e567e36
Peter Sobot <psobot@gmail.com>
2015-01-26 15:20:09 -0500

    Added comments to Secrets.example.swift to help onboarding.

--- Demo Recorder/Secrets.example.swift
+++ Demo Recorder/Secrets.example.swift
@@ -12,8 +12,20 @@
 import Foundation
 
 class Secrets {
+
+    //  This is the S3 bucket that videos will be uploaded to.
     class var AWSS3Bucket: NSString { return "mybucket" }
+
+    //  This is the prefix that videos will be uploaded to in that bucket.
+    //  With these defaults, videos will be saved in s3://mybucket/foo/bar/.
+    //  Videos are titled by start time, and their filenames look like: 2015-01-01 00:00:00.mp4
     class var AWSS3KeyPrefix: NSString { return "foo/bar" }
+
+    //  AWS access credentials.
+    //  It is strongly suggested that you generate a separate IAM user for this application,
+    //  and that you give that user only the PutObject permission on the S3 bucket listed above.
     class var AWSAccessKey: NSString { return "AKI-----------------" }
     class var AWSSecretKey: NSString { return "-------------------------/-/------------" }
+
+    Remove this line once you've added in your keys above.
 }
